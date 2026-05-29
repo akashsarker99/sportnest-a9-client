3617b4124210c5b7fdaf3d267760466244bb1a3f
akashsarker99 <akashsarker2003@gmail.com>
2026-05-29 20:41:45 +0600

    featured facilities section added

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,10 +1,12 @@
 import Banner from '@/components/Banner';
+import FeaturedFacilities from '@/components/FeaturedFacilities';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
+            <FeaturedFacilities></FeaturedFacilities>
         </div>
     );
 };
---
+++ src/components/shared/FeatureCard.jsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { FaArrowRight, FaClock, FaLocationDot, FaUsers } from 'react-icons/fa6';
+
+const FeatureCard = ({facility}) => {
+     const {_id, name, facility_type, image, location, price_per_hour, capacity, available_slots } = facility;
+    return (
+        <div className="group bg-white rounded-2xl overflow-hidden border border-gray-200 hover:border-[#24B1B1] transition duration-300 hover:shadow-xl flex flex-col h-full shadow">
+      <div className="relative overflow-hidden">
+        <Image
+          src={image}
+          alt={name}
+          width={500}
+          height={300}
+          className="w-full h-56 object-cover group-hover:scale-105 transition duration-500"
+        ></Image>
+
+        <div className="absolute top-4 left-4 bg-linear-to-l from-[#24B1B1] to-[#007979] text-white text-sm font-semibold px-4 py-2 rounded-full shadow-md">
+          {facility_type}
+        </div>
+      </div>
+
+      <div className="p-6 flex flex-col flex-1">
+        <div className="flex items-start justify-between gap-4">
+          <div>
+            <h2 className="text-2xl font-bold text-slate-700 leading-snug">{name}</h2>
+
+            <div className="flex items-center gap-2 text-gray-500 mt-3">
+              <FaLocationDot className="text-[#007979]"></FaLocationDot>
+              <p>{location}</p>
+            </div>
+          </div>
+
+          <div className="text-right shrink-0">
+            <h3 className="text-xl font-bold bg-linear-to-l from-[#24B1B1] to-[#007979] bg-clip-text text-transparent">
+              {price_per_hour} TK
+            </h3>
+            <p className="text-sm text-gray-400">per hour</p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between text-gray-600 mt-6">
+          <div className="flex items-center gap-2">
+            <FaUsers className="text-[#007979]"></FaUsers>
+            <p>{capacity} Players</p>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <FaClock className="text-[#007979]"></FaClock>
+            <p>{available_slots[0]}</p>
+          </div>
+        </div>
+
+        <div className="mt-auto pt-6 flex justify-end">
+          <Link href={`/facility/${_id}`}>
+            <button className="px-6 py-3 rounded-xl bg-linear-to-l from-[#24B1B1] to-[#007979] text-white font-semibold transition duration-300 shadow-md hover:scale-103 flex items-center gap-2">
+              Book Now
+              <FaArrowRight></FaArrowRight>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default FeatureCard;
